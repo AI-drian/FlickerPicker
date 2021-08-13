f1d59b5209b5558dd24459d0d77b9027b8d498c9
AI-drian <adrian.andreasson@outlook.com>
2021-08-13 15:13:07 +0200

    Search, todolist, styling

--- flicker-picker/src/App.js
+++ flicker-picker/src/App.js
@@ -2,16 +2,18 @@ import React, {useEffect, useState} from "react";
 
 import Movie from "./components/Movie";
 
-// For security, try and hide the API key...
+// For the future, try and hide the API key...
 const API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=963a336b03a442a36f37bffcce32b2be&page=1";
 const image_API = "http://image.tmdb.org/t/p/w500";
 const search_API = "https://api.themoviedb.org/3/search/movie?&api_key=963a336b03a442a36f37bffcce32b2be&query=";
 
 
-// This is the important part :)
+
 function App() {
   const [ movies, setMovies ] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
+  //Getting the data from API
   useEffect(() => {
     fetch(API).then(res => res.json())     
     .then(movieData => {
@@ -20,11 +22,44 @@ function App() {
     });
   }, []);
 
-    return  <div>
-      {movies.length > 0 && movies.map(movie =>   //Error "Movies is not a function" solved by returning a list > 0 &&...
-        (<Movie />
-        ))}
-    </div>;
+  const handleOnSubmit = (e) => {
+    e.preventDefault()
+
+    fetch(search_API + searchTerm)
+    .then(res => res.json())     
+    .then(movieData => {
+      setMovies(movieData.results);
+    });
+  }; 
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  }
+
+// "<> </>" == React fragments, are used for a component to return multiple elements.
+    return( 
+        <> 
+
+        <header>
+          <form onSubmit={handleOnSubmit}>
+             <input className="search-bar" 
+                    type="text" 
+                    placeholder="Search..." 
+                    value={searchTerm}
+                    onChange={handleOnChange}></input>
+
+          </form>
+
+        </header>
+
+      <div className="movie-container">
+        {movies.length > 0 && movies.map(movie =>   //Error "Movies is not a function" solved by returning a list > 0 &&...
+         (<Movie key={movie.id} {...movie} />  //Using id as key to get data from the movie, ... is called the Spread Syntax in JS
+       ))}
+      </div>
+
+      </>
+    );
 }
 
 export default App;
--- flicker-picker/src/components/Movie.js
+++ flicker-picker/src/components/Movie.js
@@ -1,8 +1,38 @@
 import React from "react";
 
-const Movie = () =>
+
+const image_API = "http://image.tmdb.org/t/p/w500";
+
+// The if statment is used to put the rating-scores in categories that can be insdividually styled
+const setRatingClass = (vote) => {
+    if (vote >= 7.5) {
+        return "green";
+    }
+    else if(vote >= 5.5) {
+        return "yellow";
+    }
+    else{
+        return "red";
+    }
+};
+
+const Movie = ({title, vote_average, overview, poster_path, release_date}) => (
     <div className ="movie">
-        Movie  
-    </div>;
+        <img src = {image_API + poster_path} alt={title}/>
+        <div className = "movie-synopsis">
+            <h3>{title}</h3> 
+            {/* <span>{release_date}</span> */}
+            <span className={
+                `tag ${setRatingClass(vote_average)}`
+                }>
+                {vote_average}</span>
+        </div>
+        
+        <div className="movie-banner">
+            <h2>Overview:</h2>
+            <p>{overview}</p>
+            </div>
+    </div>
+);
 
 export default Movie;
