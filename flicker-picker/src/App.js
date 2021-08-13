0ae4f95850186f5be917d4148b741a78ca32e260
AI-drian <adrian.andreasson@outlook.com>
2021-08-13 10:24:07 +0200

    Fetch from TMDb  API

--- flicker-picker/src/App.js
+++ flicker-picker/src/App.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 import Movie from "./components/Movie";
 
+// For security, try and hide the API key...
+const API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=963a336b03a442a36f37bffcce32b2be&page=1";
+const image_API = "http://image.tmdb.org/t/p/w500";
+const search_API = "https://api.themoviedb.org/3/search/movie?&api_key=963a336b03a442a36f37bffcce32b2be&query=";
+
+
+// This is the important part :)
 function App() {
-  const movies  = ["1","2","3", "4"];
+  const [ movies, setMovies ] = useState([]);
+
+  useEffect(() => {
+    fetch(API).then(res => res.json())     
+    .then(movieData => {
+      console.log(movieData)  //Test
+      setMovies(movieData.results);
+    });
+  }, []);
 
     return  <div>
-      {movies.map(movie => 
+      {movies.length > 0 && movies.map(movie =>   //Error "Movies is not a function" solved by returning a list > 0 &&...
         (<Movie />
         ))}
     </div>;
--- flicker-picker/src/components/Movie.js
+++ flicker-picker/src/components/Movie.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Movie = () =>
     <div className ="movie">
-        This is a movie component  
+        Movie  
     </div>;
 
 export default Movie;
