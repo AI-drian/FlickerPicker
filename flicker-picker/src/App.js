b54edb4a5aece76478395fa61fa86b5c19855899
AI-drian <adrian.andreasson@outlook.com>
2021-08-18 17:04:20 +0200

    Started on routes

--- flicker-picker/src/App.js
+++ flicker-picker/src/App.js
@@ -1,60 +1,33 @@
 import React, {useEffect, useState} from "react";
-import Movie from "./components/Movie/Movie";
+import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar"
-
-// For the future, try and hide the API key...
-const API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=963a336b03a442a36f37bffcce32b2be&page=1";
-const image_API = "http://image.tmdb.org/t/p/w500";
-const search_API = "https://api.themoviedb.org/3/search/movie?&api_key=963a336b03a442a36f37bffcce32b2be&query=";
+import About from "./components/About/About"
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 
 
 function App() {
-  const [ movies, setMovies ] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-
-  //Getting the data from API
-  useEffect(() => {
-    fetch(API).then(res => res.json())     
-    .then(movieData => {
-      console.log(movieData)  //Test
-      setMovies(movieData.results);
-    });
-  }, []);
-
-  const handleOnSubmit = (e) => {
-    e.preventDefault()
-
-    fetch(search_API + searchTerm)
-    .then(res => res.json())     
-    .then(movieData => {
-      setMovies(movieData.results);
-    });
-  }; 
-
-  const handleOnChange = (e) => {
-    setSearchTerm(e.target.value);
-  }
 
 // "<> </>" == React fragments, are used for a component to return multiple elements.
     return( 
+      <Router>
         <>   
-        <header>
-          <Navbar />
-        </header>
-      
-      <div className ="search-bar">
-        <form onSubmit={handleOnSubmit}>
-         < input className="search-bar" type="text"  placeholder="Search..." value={searchTerm} onChange={handleOnChange}></input>
-        </form>
-      </div>
+          <header>
+            <Navbar />
+          </header>
 
-      <div className="movie-container">
-        {movies.length > 0 && movies.map(movie =>   //Error "Movies is not a function" solved by returning a list > 0 &&...
-        (<Movie key={movie.id} {...movie} />  //Using id as key to get data from the movie, ... is called the Spread Syntax in JS
-       ))}
-      </div>
-      </>
+          <div className ="content">
+            <Switch>
+              <Route path="/">
+               <Home />
+              </Route>
+              <Route path="/about">
+               <About />
+              </Route>
+            </Switch>
+          </div> 
+        </>
+    </Router>
     );
 }
 
--- flicker-picker/src/components/Navbar/Navbar.js
+++ flicker-picker/src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { MenuItems } from "./MenuItems"
 import "./Navbar.css"
-import { Button } from "./Button"
+import { Button } from "./Button";
 
 class Navbar extends Component{
     // Changing burger icon when clicked
@@ -12,25 +12,23 @@ class Navbar extends Component{
 
     render(){
         return(
-            
-            <nav className="NavbarItems">
-                <h1 className="navbar-logo">FlickerPicker <i className="fas fa-fast-forward"></i></h1>
-                <div className="menu-icon" onClick ={this.handleClick}>
-                    <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
-                </div>
-                <ul className = {this.state.clicked ? "nav-menu active" : "nav-menu"}>
-                    {MenuItems.map((item, index) => {
-                        return (
-                            <li key={index}>
-                                <a className={item.cName} href={item.url}>
-                                    {item.title}
-                                </a></li>
-                           
-                        )
-                    })}
-                </ul>
-                <Button>Recommengine</Button>
-            </nav>
+                <nav className="NavbarItems">
+                    <h1 className="navbar-logo">FlickerPicker <i className="fas fa-fast-forward"></i></h1>
+                    <div className="menu-icon" onClick ={this.handleClick}>
+                        <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+                     </div>
+                    <ul className = {this.state.clicked ? "nav-menu active" : "nav-menu"}>
+                        {MenuItems.map((item, index) => {
+                            return (
+                                <li key={index}>
+                                    <a className={item.cName} href={item.url}>
+                                        {item.title}
+                                    </a></li>  
+                            )
+                        })}
+                    </ul>
+                    <Button>Recommengine</Button>
+                </nav>
        )
     }
 }
